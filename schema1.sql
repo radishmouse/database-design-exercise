dbafc5aaf714d97f16c002c2441dc8abb16d1d53
mkdir-helen <experiment.helen@gmail.com>
2018-10-31 16:05:52 -0400

    Added a new sql with users and their blog posts

---
+++ schema1.sql
@@ -0,0 +1,14 @@
+-- An user has many blog posts
+
+create table users (
+  id serial primary key,
+  name text
+);
+
+create table posts (
+  id serial primary key,
+  title text,
+  date timestamp,
+  user_id integer references users (id)  
+);
+
