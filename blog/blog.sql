986eab89d8f6a73c9a9a4b035e12befb341dc250
Ian Hundere <ianfundere@gmail.com>
2018-11-02 09:52:27 -0400

    fixes mistakes

--- blog/blog.sql
+++ blog/blog.sql
@@ -1,11 +1,13 @@
-create table users (
-    user_id serial primary key,
+create table users
+(
+    id serial primary key,
     username text
 );
 
-create table posts (
-    post_id serial primary key,
+create table posts
+(
+    id serial primary key,
     post text,
     post_time timestamp,
-    user_id integer references users(user_id)
+    user_id integer references users(id)
 );
